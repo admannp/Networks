bedd4152a16366cd734de47782aec6572fca5fcd
Nick Adman <admannp@uw.edu>
2014-04-27 14:39:21 -0700

    hopefully proxy in working state

--- src/Proxy/Proxy.java
+++ src/Proxy/Proxy.java
@@ -28,11 +28,13 @@ public class Proxy {
 			int port;
 			String header = parseHeader(inFromClient);
 			Scanner headScan = new Scanner(header);
+			
+			// Find the host information
 			while (headScan.hasNextLine()) {
 				String line = headScan.nextLine();
 				Scanner lineScan = new Scanner(line);
 				String tag = lineScan.next();
-				if (tag.equals("Host: ")) {
+				if (tag.equals("Host:")) {
 					host = lineScan.next();
 				}
 			}
@@ -41,6 +43,9 @@ public class Proxy {
 			host = hostInformation[0];
 			port = (hostInformation.length > 1) ? 
 					Integer.parseInt(hostInformation[1]) : 80;
+					
+			System.out.println("Host: " + host);
+			System.out.println("Port: " + port);
 			
 
 			
@@ -49,17 +54,23 @@ public class Proxy {
 			// TODO: Don't change this name
 			
 			// Create socket connected to server, set up output stream
-			Socket socketDaServerBetch = new Socket(host, port);
+			Socket serverSocket = new Socket(host, port);
 			DataOutputStream outToServer = new DataOutputStream(
-					socketDaServerBetch.getOutputStream());
+					serverSocket.getOutputStream());
 			// Send header, along with double new-line
 			outToServer.writeBytes(header.toString() + "\r\n\r\n");
 			
 			
+			
+			
+			
+			BufferedReader serverHeaderReader = new BufferedReader(new InputStreamReader(serverSocket.getInputStream()));
+			header = parseHeader(serverHeaderReader);
+			
+			outToClient.writeBytes(header.toString() + "\r\n\r\n");
+			
 			// Input stream from server
-			DataInputStream inFromServer = new DataInputStream(socketDaServerBetch.getInputStream());
-			
-			
+			DataInputStream inFromServer = new DataInputStream(serverSocket.getInputStream());
 			
 			ByteArrayOutputStream baos = new ByteArrayOutputStream();
 			byte buffer[] = new byte[1024];
@@ -89,23 +100,14 @@ public class Proxy {
 	// the host information
 	public static String parseHeader(BufferedReader input) throws IOException {
 		StringBuffer header = new StringBuffer();
-		
-		// To store length of body following header
-		int contentLength = 0;
-		// For the host
-		String host = "";
-		int port = -1;
 		String line = input.readLine();
-		// Used to check to make sure we read any data at all
-		boolean hasData = false;
-		
+
 		if (line == null) {
 			System.err.println("The page is cached");
 			return "";
 		}
 		while (line.length() > 0) {
 			Scanner lineScan = new Scanner(line);
-			hasData = true;
 			line = line.trim();
 			String tag = lineScan.next();
 			if (tag.equals("Connection:")) {
@@ -113,26 +115,10 @@ public class Proxy {
 				if (!connectionType.equals("close")) {
 					line = "Connection: close";
 				}
-			} /* else if (tag.equals("Host:")) {
-				// First index will be hostname--second, if there, will be port
-				String[] hostInformation = lineScan.next().split(":");
-				host = hostInformation[0];
-				port = (hostInformation.length > 1) ? 
-						Integer.parseInt(hostInformation[1]) : 80;
-			} else if (tag.equals("Content-Length:")) {
-				contentLength = lineScan.nextInt();
-			} */
+			} 
 			header.append(line + "\n");
 			line = input.readLine();
 			lineScan.close();
-		}
-		
-		// Checking to make sure we've captured required information
-		if (!hasData || contentLength == 0 || host.equals("")) {
-			System.err.println("Invalid header");
-			System.err.println("Has data: " + hasData);
-			System.err.println("Content Length: " + contentLength);
-			System.err.println("Host: " + host);
 		}
 		
 		return header.toString();
