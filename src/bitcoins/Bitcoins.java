7bcd69d94fe6f5b1862c1e7e00ab08dbea8c6700
Nick Adman <admannp@uw.edu>
2014-06-07 15:50:56 -0700

    Hi I am Nick hahahaha

--- src/bitcoins/Bitcoins.java
+++ src/bitcoins/Bitcoins.java
@@ -9,8 +9,11 @@ import java.nio.file.Paths;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.Security;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
+
+import javax.xml.bind.DatatypeConverter;
 
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
@@ -34,6 +37,8 @@ public class Bitcoins {
 		
 		Security.addProvider(new BouncyCastleProvider());
 		
+
+		
 		
 		// changed 	"new MessageDigest.getInstance("SHA-256")"
 		// to 		"MessageDigest.getInstance("SHA-256")"
@@ -45,65 +50,67 @@ public class Bitcoins {
 		byte[] hash = md.digest();
 		System.out.println(bytesToHex(hash));
 		System.out.println("Size of hash: " + hash.length);*/
+
+		System.out.println("Library solution: " + DatatypeConverter.printHexBinary(hash("-----BEGIN RSA PUBLIC KEY-----\n" + 
+				"MIGJAoGBAN3MxXHcbc1VNKTOgdm7W+i/dVnjv8vYGlbkdaTKzYgi8rQm126Sri87\n" + 
+				"702UBNzmkkZyKbRKL/Bfc4EG8/Mt9Pd2xQlRyXCL9FnIFWHyhfIQtW+oBsGI5UhG\n" +
+				"I8B8MiPOMfb6d/PdK+vd4riUxHAvCkHW5Lw0szAD1RVGbkG/7qnzAgMBAAE=\n" +
+				"-----END RSA PUBLIC KEY-----")).toLowerCase());
+		
+	}
+	
+	private static byte[] hash(byte[] hashMe) {
+		md.update(hashMe);
+		md.update(md.digest());
+		return md.digest();
+	}
+	
+	private static byte[] hash(String hashMe) {
+		return hash(hashMe.getBytes());
 	}
 	
 	private static byte[] merkleRootComputation(byte[][] data) {
 		// We use a queue to store the hashes as the tree is processed
 		Queue<byte[]> hashes = new LinkedList<byte[]>();
 		for (byte[] entry : data) {
-			md.update(entry);
-			byte[] hash = md.digest();
-			hashes.add(hash);
+			hashes.add(hash(entry));
 			md.reset();
 		}
+		
 		// If there are an odd number of entries, add the last one twice
 		if (data.length % 2 == 1) {
-			md.update(data[data.length - 1]);
-			byte[] hash = md.digest();
-			hashes.add(hash);
+			hashes.add(hash(data[data.length - 1]));
 			md.reset();
 		}
+		
 		// Our end condition is that there is just one item left in the queue.
 		// When this is true, return that element.
 		while(hashes.size() > 1) {
 			// Take the front two entries, concatenate them, hash the result,
 			// and then add it back into the queue.
 			System.out.println("--- Beginning iteration ---");
-			byte[] hash = null;
+			byte[] concatenation = null;
 			System.out.println("Beginning size of queue: " + hashes.size());
 			int currentSize = hashes.size();
 			for (int i = 0; i < currentSize / 2; i++) {
 				byte[] firstEntry = hashes.remove();
-				System.out.println("Removed: " + bytesToHex(firstEntry));
+				System.out.println("Removed: " + DatatypeConverter.printHexBinary(firstEntry));
 				byte[] secondEntry = hashes.remove();
-				System.out.println("Removed: " + bytesToHex(secondEntry));
-				byte[] concatenation = new byte[firstEntry.length + secondEntry.length];
+				System.out.println("Removed: " + DatatypeConverter.printHexBinary(secondEntry));
+				concatenation = new byte[firstEntry.length + secondEntry.length];
 				System.arraycopy(firstEntry, 0, concatenation, 0, firstEntry.length);
 				System.arraycopy(secondEntry, 0, concatenation, firstEntry.length, secondEntry.length);
-				md.update(concatenation);
-				hash = md.digest();
-				hashes.add(hash);
-				System.out.println("Added: " + bytesToHex(hash));
+				hashes.add(hash(concatenation));
+				System.out.println("Added: " + DatatypeConverter.printHexBinary(hash(concatenation)));
 				md.reset();
 			}
 			// If the size is odd, add in the last one again
 			if (hashes.size() % 2 == 1 && hashes.size() != 1) {
-				hashes.add(hash);
+				hashes.add(hash(concatenation));
 			}
 			System.out.println("Ending size of queue: " + hashes.size());
 		}
 		return hashes.remove();
-	}
-	
-	final protected static char[] hexArray = "0123456789abcdef".toCharArray();
-	public static String bytesToHex(byte[] bytes) {
-	    char[] hexChars = new char[bytes.length * 2];
-	    for ( int j = 0; j < bytes.length; j++ ) {
-	        int v = bytes[j] & 0xFF;
-	        hexChars[j * 2] = hexArray[v >>> 4];
-	        hexChars[j * 2 + 1] = hexArray[v & 0x0F];
-	    }
-	    return new String(hexChars);
 	}
 
 }
